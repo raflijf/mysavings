ac59ade1de6b1004dd4d495df35bb602296e5f3f
Rafli <raflijf7@gmail.com>
2025-05-12 19:11:46 +0700

    -

--- main/templatetags/template_tags_saving.py
+++ main/templatetags/template_tags_saving.py
@@ -1,6 +1,6 @@
 from django import template
 from main.models  import Saving
-
+from decimal import Decimal
 
 register = template.Library()
 
@@ -10,7 +10,19 @@ def status(id_saving) :
     status = saving.saving_entries.all().order_by('-datetime').first()
     return status
 
+
+def safe_int(value):
+    # Jika value adalah None atau string kosong, kembalikan 0
+    if value in (None, ''):
+        return 0
+    try:
+        # Coba konversi ke integer
+        return int(value)
+    except ValueError:
+        # Jika terjadi error konversi, kembalikan 0
+        return 0
+
 @register.simple_tag
 def gap(target, collected) :
-    return target - collected
+    return safe_int(target) - safe_int(collected)
 
